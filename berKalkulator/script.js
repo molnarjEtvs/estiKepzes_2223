9a9bce577a36dea2f597d6ad1ac93f4da18718a7
Molnár Tamás <molnarj@EOTVOS2.LOCAL>
2022-12-12 16:37:36 +0100

    bérkalkulátor kész

--- berKalkulator/script.js
+++ berKalkulator/script.js
@@ -1,3 +1,21 @@
 function kiszamol(){
-    console.log("működik");
+    var bruttoBer = parseInt(document.getElementById("brutto_ber").value);
+    var kor = parseInt(document.getElementById("kor").value);
+    var tb = bruttoBer*0.185;
+    //SZJA = Bruttó bér 15% -a (HA 25 alatti akkor ez a z érték 0!) 
+    var szja = bruttoBer*0.15;
+    if(kor == "0"){
+       szja = 0;
+    }
+    //Nettó bér = Bruttó bér-SZJA-TB 
+    var nettoBer = bruttoBer-szja-tb;
+    var szocho = bruttoBer*0.13;
+    var munkKtsg = bruttoBer+szocho;
+
+    document.getElementById("nettoEredmeny").innerHTML = nettoBer;
+    document.getElementById("szjaEredmeny").innerHTML = szja;
+    document.getElementById("tbEredmeny").innerHTML = tb;
+    document.getElementById("mktsgEredmeny").innerHTML = munkKtsg;
+    
+
 }
